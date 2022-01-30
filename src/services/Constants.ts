2080efda6e6cc3c19922a87e348527e82f723ee1
MPIZM <mpizman@gmail.com>
2022-01-30 18:33:32 +0200

    remove key

--- src/services/Constants.ts
+++ src/services/Constants.ts
@@ -1,7 +1,7 @@
 
 class Constants {
-  public static ACCESS_KEY_AWS = "AKIAQ5MURWZXBN4EXKHH";
-  public static SECRET_ACCESS_KEY_AWS = "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";
+  public static ACCESS_KEY_AWS = "";
+  public static SECRET_ACCESS_KEY_AWS = "";
 }
 
 export default Constants;
